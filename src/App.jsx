226781eb54bd132be827d946db723f2c2298df1e
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-11-18 00:48:58 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Container from './components/Container';
+
+function App() {
+
+
+  return (
+  <>
+  <section>
+    <Container>
+    <h1>project</h1>
+  </Container>
+  </section>
+  </>
+  )
+}
+
+export default App
